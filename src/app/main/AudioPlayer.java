42537401cb41a0583fc56267193a7aa3338287cd
AyalaLazaro05 <lazaritoayala05@gmail.com>
2025-05-02 14:39:21 +0200

    Harry to esto ta modularizado ya. Cuatro clases nuevas: una para cada menu; el menu principal, el menu de opciones y el menu de modos de juego, y una para todos los sliders. Amen. Dios bendiga a nuestro codigo, a la madre Patria y a tus benditos cojones.

--- src/app/main/AudioPlayer.java
+++ src/app/main/AudioPlayer.java
@@ -1,42 +1,43 @@
 package app.main;
 
+import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 import java.nio.file.Paths;
+import java.text.Normalizer;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class AudioPlayer {
     private static final Map<String, MediaPlayer> sounds = new HashMap<>();
-    private static double volume = 0.4;
-    private static Map<String, MediaPlayer> activeSounds = new HashMap<>();
+    private static final Map<String, AudioClip> clips = new HashMap<>();
+    private static double MusicVolumen = 0.4;
+    private static double ClipsVolumen = 0.4;
+    private static final Map<String, MediaPlayer> activeSounds = new HashMap<>();
+    private static final Map<String, AudioClip> activeClips = new HashMap<>();
 
-    public static void init() {
-
-        sounds.put("selectedCharacter", new MediaPlayer(new Media(Paths.get("src/DAO/audios/sound_effects/Personajeseleccionao.mp3").toUri().toString())));
+    public static void initMediaPlayer() {
         sounds.put("mainMenu", new MediaPlayer(new Media(Paths.get("src/DAO/audios/music/mainMenu.mp3").toUri().toString())));
-        sounds.put("buttonSound", new MediaPlayer(new Media(Paths.get("src/DAO/audios/sound_effects/buttonMenu.mp3").toUri().toString())));
         sounds.put("combatMusic", new MediaPlayer(new Media(Paths.get("src/DAO/audios/music/combatMusic.mp3").toUri().toString())));
-        sounds.put("roasterButton", new MediaPlayer(new Media(Paths.get("src/DAO/audios/sound_effects/roasterButtonActivated.mp3").toUri().toString())));
 
         for (MediaPlayer player : sounds.values()) {
-            player.setVolume(volume);
+            player.setVolume(MusicVolumen);
         }
     }
 
-    public static void playInit(){
-
+    public static void initAudioClips() {
+        clips.put("selectedCharacter", new AudioClip(Paths.get("src/DAO/audios/sound_effects/Personajeseleccionao.mp3").toUri().toString()));
+        clips.put("buttonSound", new AudioClip(Paths.get("src/DAO/audios/sound_effects/buttonMenu.mp3").toUri().toString()));
+        clips.put("roasterButton", new AudioClip(Paths.get("src/DAO/audios/sound_effects/roasterButtonActivated.mp3").toUri().toString()));
     }
 
     public static void playSelectedCharacter() {
-        stopIfPlaying("selectedCharacter");
-        MediaPlayer player = sounds.get("selectedCharacter");
-        if (player != null) {
-            player.setVolume(volume);
-            player.play();
-            activeSounds.put("selectedCharacter", player);
+        AudioClip clip = clips.get("selectedCharacter");
+        if (clip != null) {
+            clip.setVolume(ClipsVolumen);
+            clip.play();
+            activeClips.put("selectedCharacter", clip);
         }
     }
 
@@ -45,12 +46,11 @@ public class AudioPlayer {
         MediaPlayer player = sounds.get("mainMenu");
         if (player != null) {
             player.setCycleCount(MediaPlayer.INDEFINITE);
-            player.setVolume(volume);
+            player.setVolume(MusicVolumen);
             player.play();
             activeSounds.put("mainMenu", player);
         }
     }
-
 
     public static void stopMainMenu() {
         MediaPlayer player = activeSounds.get("mainMenu");
@@ -61,33 +61,29 @@ public class AudioPlayer {
     }
 
     public static void playButtonSound() {
-        stopIfPlaying("buttonSound");
-        MediaPlayer player = sounds.get("buttonSound");
-        if (player != null) {
-            player.setVolume(volume);
-            player.play();
-            activeSounds.put("buttonSound", player);
+        AudioClip clip = clips.get("buttonSound");
+        if (clip != null) {
+            clip.setVolume(ClipsVolumen);
+            clip.play();
+            activeClips.put("buttonSound", clip);
         }
     }
 
     public static void playRoasterButtonSound() {
-        stopIfPlaying("roasterButton");
-        MediaPlayer player = sounds.get("roasterButton");
-        if (player != null) {
-            player.setVolume(volume);
-            player.play();
-            activeSounds.put("roasterButton", player);
-
+        AudioClip clip = clips.get("roasterButton");
+        if (clip != null) {
+            clip.setVolume(ClipsVolumen);
+            clip.play();
+            activeClips.put("roasterButton", clip);
         }
     }
-
 
     public static void playCombatMusic() {
         stopIfPlaying("combatMusic");
         MediaPlayer player = sounds.get("combatMusic");
         if (player != null) {
             player.setCycleCount(MediaPlayer.INDEFINITE);
-            player.setVolume(volume);
+            player.setVolume(MusicVolumen);
             player.play();
             activeSounds.put("combatMusic", player);
         }
@@ -102,13 +98,28 @@ public class AudioPlayer {
     }
 
     public static void setVolumeForAllSounds(double newVolume) {
-        volume = newVolume;
-        System.out.println("Nuevo volumen: " + volume);
-
+        MusicVolumen = newVolume;
+        ClipsVolumen = newVolume;
 
         for (MediaPlayer player : activeSounds.values()) {
-            player.setVolume(volume);
+            player.setVolume(MusicVolumen);
+        }
+        for (AudioClip clip : activeClips.values()) {
+            clip.setVolume(ClipsVolumen);
         }
     }
 
+    public static void setVolumenForMusic(double newVolumen) {
+        MusicVolumen = newVolumen;
+        for (MediaPlayer player: activeSounds.values()){
+            player.setVolume(MusicVolumen);
+        }
+    }
+
+    public static void setVolumenForEffect(double newVolumen){
+        ClipsVolumen = newVolumen;
+        for (AudioClip clip: activeClips.values()){
+            clip.setVolume(ClipsVolumen);
+        }
+    }
 }
--- src/app/main/Game.java
+++ src/app/main/Game.java
@@ -1,18 +1,20 @@
 package app.main;
 
-import app.main.Roaster.Roaster;
+import app.menus.GameModeMenu;
+import app.menus.MainMenu;
+import app.menus.OptionsMenu;
+import app.menus.SlidersSoundsMenu;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
-import javafx.scene.control.Slider;
+
+import java.util.Collection;
 // Cosas de JavaFX.
 
 
@@ -20,28 +22,12 @@ public class Game extends Application {
     public static Stage window;
     public static AudioClip audio;
     private Scene mainScene;
-    private VBox mainMenu;
+    private StackPane stackPane=new StackPane();
+    private HBox mainContainer;
+    private VBox leftContainer;
+    private StackPane rightContainer;
     // Escena, graficos, raiz y lienzo activados para su uso en toda la aplicacion.
 
-
-    private Button play;
-    private Button options;
-    private Button quit;
-    private Button atras;
-    private Button campaign = new Button();
-    private Button pvp = new Button();
-    private Button tournament = new Button();
-    private Button Audio;
-    private Button video;
-    private Button controles;
-
-
-    private Slider volumen;
-    private HBox sliderContainer;;
-
-
-
-    // Botones y Labels.
 
     public static void main(String[] args) {
         launch(args);
@@ -53,202 +39,52 @@ public class Game extends Application {
         /* Game.window es la variable publica estatica que usaran
         todas las clases para instanciar la ventana que necesiten.
          */
-        AudioPlayer.init();
-        windowDesign();
-    }
-
-    private void windowDesign() {
+        AudioPlayer.initMediaPlayer();
+        AudioPlayer.initAudioClips();
+        MainMenu.initMainMenu();
+        GameModeMenu.initGameModeMenu();
+        OptionsMenu.initOptionsMenu();
+        SlidersSoundsMenu.initSlidersForVolumen();
         AudioPlayer.playMainMenu();
 
-        Image fondoPrincipal= new Image("fondoPrincipal.png");
-        ImageView vista = new ImageView(fondoPrincipal);
-        play = new Button("Jugar");
-        play.getStyleClass().add("menu-button");
-        options = new Button("Opciones");
-        options.getStyleClass().add("menu-button");
-        quit = new Button("Salir");
-        quit.getStyleClass().add("menu-button");
-        campaign = new Button("Campaña.");
-        campaign.getStyleClass().add("menu-button");
-        pvp = new Button("PvP.");
-        pvp.getStyleClass().add("menu-button");
-        tournament = new Button("Torneo.");
-        tournament.getStyleClass().add("menu-button");
-        Audio=new Button("Audio");
-        Audio.getStyleClass().add("menu-button");
-        atras=new Button("Atras");
-        atras.getStyleClass().add("menu-button");
-        video = new Button("Video");
-        video.getStyleClass().add("menu-button");
-        controles = new Button("Controls");
-        controles.getStyleClass().add("menu-button");
+        ImageView wallpaper=new ImageView(getClass().getResource("/fondoPrincipal.png").toExternalForm());
+        wallpaper.setPreserveRatio(false);
+        wallpaper.fitWidthProperty().bind(stackPane.widthProperty());
+        wallpaper.fitHeightProperty().bind(stackPane.heightProperty());
 
-        // Botones.
+        leftContainer=MainMenu.getMainMenu();
+
+        rightContainer=new StackPane();
+        rightContainer.setPrefWidth(300);
+        rightContainer.setAlignment(Pos.CENTER_RIGHT);
+        rightContainer.getChildren().addAll(OptionsMenu.getOptionsMenu(),GameModeMenu.getGameModeMenu(),
+                SlidersSoundsMenu.getSliders());
 
 
-        volumen=new Slider(0,1,0.4);
-        volumen.setShowTickLabels(true);
 
-        sliderContainer=new HBox();
-        sliderContainer.setAlignment(Pos.CENTER);
-        sliderContainer.getChildren().add(volumen);
-        sliderContainer.setPrefSize(200,20);
-        volumen.setPrefSize(200,20);
-        volumen.valueProperty().addListener((observable, oldValue, newValue) -> {
-            double newVolumen= newValue.doubleValue();
-            AudioPlayer.setVolumeForAllSounds(newVolumen);
-        });
+        mainContainer=new HBox();
+        mainContainer.setSpacing(150);
+        mainContainer.getChildren().addAll(leftContainer,rightContainer);
 
 
-        mainMenu = new VBox(10);
-        mainMenu.getChildren().addAll(play, options,quit,campaign, pvp,tournament,Audio,atras,video,controles,sliderContainer);
-        mainMenu.setAlignment(Pos.CENTER);
-
-        disableGameModesButtons();
-        disableVolumenSlider();
-        disableOptionsButton();
-
-        vista.setPreserveRatio(false);
-        vista.setFitWidth(850);
-        vista.setFitHeight(832);
-
-        StackPane root = new StackPane();
-        root.getChildren().addAll(vista, mainMenu);
-        mainScene = new Scene(root,832, 850);
-        vista.fitWidthProperty().bind(mainScene.widthProperty());
-        vista.fitHeightProperty().bind(mainScene.heightProperty());
-
-        //Traslacion de los botones en el eje de las X
-        play.translateXProperty().bind(mainScene.widthProperty().multiply(-0.30));
-        options.translateXProperty().bind(mainScene.widthProperty().multiply(-0.30));
-        quit.translateXProperty().bind(mainScene.widthProperty().multiply(-0.30));
-
-        campaign.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        pvp.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        tournament.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-
-        controles.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        video.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        Audio.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        atras.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
-        sliderContainer.translateXProperty().bind(mainScene.widthProperty().multiply(0.30));
 
 
-        //Traslacion de los botones en el eje de las Y
-        play.translateYProperty().bind(mainScene.widthProperty().multiply(0.24));
-        options.translateYProperty().bind(mainScene.widthProperty().multiply(0.24));
-        quit.translateYProperty().bind(mainScene.widthProperty().multiply(0.24));
+        stackPane.getChildren().addAll(wallpaper,mainContainer);
+        mainScene=new Scene(stackPane,832,850);
 
-        campaign.translateYProperty().bind(mainScene.widthProperty().multiply(0.03));
-        pvp.translateYProperty().bind(mainScene.widthProperty().multiply(0.03));
-        tournament.translateYProperty().bind(mainScene.widthProperty().multiply(0.03));
 
-        controles.translateYProperty().bind(mainScene.widthProperty().multiply(-0.45));
-        video.translateYProperty().bind(mainScene.widthProperty().multiply(-0.31));
-        Audio.translateYProperty().bind(mainScene.widthProperty().multiply(-0.105));
-        atras.translateYProperty().bind(mainScene.widthProperty().multiply(-0.105));
-        sliderContainer.translateYProperty().bind(mainScene.widthProperty().multiply(-0.35));
-
-        mainScene.getStylesheets().add("buttons.css");
-
-        play.setOnAction(_ -> enableGameModesButtons());
-        // El boton lleva a los modos de juego.
-
-        quit.setOnAction(_ -> window.close());
-        // El boton cierra el programa.
-
-        campaign.setOnAction(_ -> initializeRoaster());
-        // El boton inicia el modo campaña.
-
-        options.setOnAction(_ -> enableOptionsButton());
-        //El boton lleva al interior del menu de opciones.
-
-        atras.setOnAction(_ -> {
-                    if (sliderContainer.isVisible()) {
-                        disableVolumenSlider();
-                        enableOptionsButton();
-                    } else if (controles.isVisible() || video.isVisible() || Audio.isVisible()) {
-                        disableOptionsButtonByBack();
-
-                    }
-                });
-        //El boton cierra el menu de opciones.
-
-        Audio.setOnAction(_ -> enableVolumenSlider());
-        //EL boton abre la barra de volumen.
-
+        mainScene.getStylesheets().add(getClass().getResource("/buttons.css").toExternalForm());
 
         window.setScene(mainScene);
-        window.setTitle("Cronicas de Valthar: El torneo de las eras");
+        window.setTitle("Cronicas de Valthar: El Torneo de las Eras");
+        window.setResizable(false);
         window.show();
-    }
 
-    private void initializeRoaster(){
-        AudioPlayer.playButtonSound();
-        AudioPlayer.stopMainMenu();
-        Roaster.initialize();
-    }
 
-    private void disableGameModesButtons(){
-        campaign.setDisable(true);
-        campaign.setVisible(false);
-        pvp.setDisable(true);
-        pvp.setVisible(false);
-        tournament.setDisable(true);
-        tournament.setVisible(false);
-    }
-
-    private void enableGameModesButtons(){
-        AudioPlayer.playButtonSound();
-        campaign.setDisable(false);
-        campaign.setVisible(true);
-        pvp.setDisable(false);
-        pvp.setVisible(true);
-        tournament.setDisable(false);
-        tournament.setVisible(true);
-        Audio.setVisible(false);
-        atras.setVisible(false);
-        controles.setVisible(false);
-        video.setVisible(false);
-    }
-
-    private void enableOptionsButton(){
-        AudioPlayer.playButtonSound();
-        campaign.setVisible(false);
-        pvp.setVisible(false);
-        tournament.setVisible(false);
-        controles.setVisible(true);
-        video.setVisible(true);
-        Audio.setVisible(true);
-        atras.setVisible(true);
-
-    }
-
-    private void disableOptionsButtonByBack(){
-        AudioPlayer.playButtonSound();
-        controles.setVisible(false);
-        video.setVisible(false);
-        Audio.setVisible(false);
-        atras.setVisible(false);
-    }
-    private void disableOptionsButton(){
-        controles.setVisible(false);
-        video.setVisible(false);
-        Audio.setVisible(false);
-        atras.setVisible(false);
-    }
-    private void enableVolumenSlider(){
-        AudioPlayer.playButtonSound();
-        controles.setVisible(false);
-        video.setVisible(false);
-        Audio.setVisible(false);
-        atras.setVisible(true);
-        sliderContainer.setVisible(true);
-    }
-    private void disableVolumenSlider(){
-        sliderContainer.setVisible(false);
 
     }
 
 }
+
+
 
